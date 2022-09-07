c09e378fdae07affaefd6aa11d66b3e9476e2198
Toshihiko Okubo <toshihiko.okubo@datachain.jp>
2022-09-07 15:49:28 +0900

    add test cases for v0.3 version of the yui-relayer
    Signed-off-by: Toshihiko Okubo <toshihiko.okubo@datachain.jp>

--- tests/chains/tendermint/simapp/simd/cmd/cmd_test.go
+++ tests/chains/tendermint/simapp/simd/cmd/cmd_test.go
@@ -6,9 +6,10 @@ import (
 
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
 	"github.com/cosmos/cosmos-sdk/x/genutil/client/cli"
-	"github.com/hyperledger-labs/yui-relayer/tests/tendermint/simapp"
-	"github.com/hyperledger-labs/yui-relayer/tests/tendermint/simapp/simd/cmd"
 	"github.com/stretchr/testify/require"
+
+	"github.com/hyperledger-labs/yui-relayer/tests/chains/tendermint/simapp"
+	"github.com/hyperledger-labs/yui-relayer/tests/chains/tendermint/simapp/simd/cmd"
 )
 
 func TestInitCmd(t *testing.T) {
