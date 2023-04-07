8ae9a6fbaa4b287a145974622baa74282af75754
Dongri Jin <dongri.jin@speee.jp>
2023-04-07 09:57:59 +0900

    Add eth2eth test
    Signed-off-by: Dongri Jin <dongri.jin@speee.jp>

--- relayer/main.go
+++ relayer/main.go
@@ -3,13 +3,17 @@ package main
 import (
 	"log"
 
+	"github.com/hyperledger-labs/yui-ibc-solidity/pkg/relay/ethereum"
 	tendermint "github.com/hyperledger-labs/yui-relayer/chains/tendermint/module"
 	"github.com/hyperledger-labs/yui-relayer/cmd"
+	mock "github.com/hyperledger-labs/yui-relayer/provers/mock/module"
 )
 
 func main() {
 	if err := cmd.Execute(
 		tendermint.Module{},
+		mock.Module{},
+		ethereum.Module{},
 	); err != nil {
 		log.Fatal(err)
 	}
