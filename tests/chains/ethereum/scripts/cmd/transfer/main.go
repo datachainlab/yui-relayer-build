67a10f912951e701d9f362541f724914de1fdae4
Masanori Yoshida <masanori.yoshida@datachain.jp>
2023-04-20 11:07:01 +0900

    delete redundant stuffs
    Signed-off-by: Masanori Yoshida <masanori.yoshida@datachain.jp>

--- tests/chains/ethereum/scripts/cmd/transfer/main.go
+++ tests/chains/ethereum/scripts/cmd/transfer/main.go
@@ -79,6 +79,12 @@ func Transfer(configDir string, fromIndex, toIndex uint32, amount int64, simpleT
 	}
 	log.Println("2. deposit success")
 
+	var heightB uint64
+	if header, err := chainB.LastHeader(ctx); err != nil {
+		log.Fatalf("failed to the latest header of chain B: %v", err)
+	} else {
+		heightB = header.Number.Uint64()
+	}
 	baseDenom := strings.ToLower(simpleTokenAddress)
 	_, err = chainA.ICS20Transfer.SendTransfer(
 		chainA.TxOpts(ctx, fromIndex),
@@ -86,7 +92,7 @@ func Transfer(configDir string, fromIndex, toIndex uint32, amount int64, simpleT
 		uint64(amount),
 		chainB.CallOpts(ctx, toIndex).From,
 		chanA.PortID, chanA.ID,
-		uint64(chainB.LastHeader().Number.Int64())+1000,
+		heightB + 1000,
 	)
 	if err != nil {
 		log.Println("sendTransfer error: ", err)
