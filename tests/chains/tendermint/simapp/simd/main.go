77370091ed511b1053b34a0ccef4f72087a794b7
Masanori Yoshida <masanori.yoshida@datachain.jp>
2023-06-30 11:09:15 +0900

    tendermint: update simapp
    Signed-off-by: Masanori Yoshida <masanori.yoshida@datachain.jp>

--- tests/chains/tendermint/simapp/simd/main.go
+++ tests/chains/tendermint/simapp/simd/main.go
@@ -13,10 +13,11 @@ import (
 func main() {
 	rootCmd, _ := cmd.NewRootCmd()
 
-	if err := svrcmd.Execute(rootCmd, simapp.DefaultNodeHome); err != nil {
+	if err := svrcmd.Execute(rootCmd, "simd", simapp.DefaultNodeHome); err != nil {
 		switch e := err.(type) {
 		case server.ErrorCode:
 			os.Exit(e.Code)
+
 		default:
 			os.Exit(1)
 		}
